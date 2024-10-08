6203f85c038145759d9d3e531f1dcbcf53994b65
miguelm7 <miguelmore100@gmail.com>
2024-10-08 11:20:59 +0200

    add html

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI App 1!"}
+# Crear un objeto Jinja2Templates para cargar las plantillas HTML
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def leer_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
